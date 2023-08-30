08f8b09c6e9ed46e8388cdb3282e8ef93fe6bcb9
EduardoMeng <143565595+EduardoMeng@users.noreply.github.com>
2023-08-30 00:37:33 -0300

    segundo commit

---
+++ teste_flet.py
@@ -0,0 +1,89 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Flet counter example"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    page.add(ft.Row([ft.IconButton(ft.icons.REMOVE, on_click=minus_click),txt_number,ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
+
+ft.app(target=main)
+
+'''
+import flet as ft
+
+def main(page):
+    def add_clicked(e):
+        page.add(ft.Checkbox(label=new_task.value))
+        new_task.value = ""
+        new_task.focus()
+        new_task.update()
+
+    new_task = ft.TextField(hint_text="Whats needs to be done?", width=300)
+    page.add(ft.Row([new_task, ft.ElevatedButton("Add", on_click=add_clicked)]))
+
+ft.app(target=main)
+
+
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Drag and Drop example"
+
+    def drag_accept(e):
+        # get draggable (source) control by its ID
+        src = page.get_control(e.src_id)
+        # update text inside draggable control
+        src.content.content.value = "0"
+        # update text inside drag target control
+        e.control.content.content.value = "1"
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.Draggable(
+                    group="number",
+                    content=ft.Container(
+                        width=50,
+                        height=50,
+                        bgcolor=ft.colors.CYAN_200,
+                        border_radius=5,
+                        content=ft.Text("1", size=20),
+                        alignment=ft.alignment.center,
+                    ),
+                ),
+                ft.Container(width=100),
+                ft.DragTarget(
+                    group="number",
+                    content=ft.Container(
+                        width=50,
+                        height=50,
+                        bgcolor=ft.colors.PINK_200,
+                        border_radius=5,
+                        content=ft.Text("0", size=20),
+                        alignment=ft.alignment.center,
+                    ),
+                    on_accept=drag_accept,
+                ),
+            ]
+        )
+    )
+
+ft.app(target=main)
+
+'''
